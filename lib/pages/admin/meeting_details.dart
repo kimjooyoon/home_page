3869b57e805aecb8aac79ff9f90ac9f250946910
teddy <teddy@sentbe.com>
2024-06-03 16:52:15 +0900

    fix(admin): modify meeting detail from temp

--- lib/pages/admin/meeting_details.dart
+++ lib/pages/admin/meeting_details.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:home_page/organisms/lists/meeting_list.dart';
+import 'package:home_page/templates/base/base_template.dart';
 
 class MeetingDetailsPage extends StatelessWidget {
   const MeetingDetailsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Meeting Details'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: MeetingList(
-          itemCount: 10,
-          onEdit: () {
-            // 미팅 내용 수정 로직
-          },
-        ),
+    return BaseTemplate(
+      title: 'Meeting Details',
+      child: MeetingList(
+        itemCount: 10,
+        onEdit: () {
+          // 미팅 내용 수정 로직
+        },
       ),
     );
   }
